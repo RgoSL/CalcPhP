b318734d68a6e6f7463d38d1d6caae6635d15340
RgoSL <125403873+RgoSL@users.noreply.github.com>
2024-04-28 00:36:20 -0300

    PUSH

---
+++ AtivCalc/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Calculadora 🗿</title>
+    
+</head>
+<body>
+        <div class="Calc">
+            <form action="" method="post">
+                <br>
+                <input type="text" class="maininput"> <br> <br>
+                <input type="submit" class="numbtn" name="num"value="7">
+                <input type="submit" class="numbtn" name="num"value="8">
+                <input type="submit" class="numbtn" name="num"value="9">
+                <input type="submit" class="calbtn" name="op"value="+"> <br>
+                <input type="submit" class="numbtn" name="num"value="4">
+                <input type="submit" class="numbtn" name="num"value="5">
+                <input type="submit" class="numbtn" name="num"value="6">
+                <input type="submit" class="calbtn" name="op"value="-"> <br>
+                <input type="submit" class="numbtn" name="num"value="1">
+                <input type="submit" class="numbtn" name="num"value="2">
+                <input type="submit" class="numbtn" name="num"value="3">
+                <input type="submit" class="calbtn" name="op"value="*"> <br>
+                <input type="submit" class="c" name=""value="c">
+                <input type="submit" class="numbtn" name="num"value="0">
+                <input type="submit" class="equal" name ="equal"value="=">
+                <input type="submit" class="calbtn" name ="op"value="/">
+                
+            </form>
+        </div>
+</body>
+</html>
